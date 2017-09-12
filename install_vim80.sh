d0360563097708791fc454e857fe532d2a8be9fb
superlc320 <luocan14@gmail.com>
2017-09-13 01:13:52 +0800

    add install_vim80.sh

---
+++ install_vim80.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo '123456' | sudo -S yum -y install ncurses-devel
+
+mkdir -p ~/download
+cd ~/download
+
+wget -c https://github.com/vim/vim/archive/v8.0.1095.tar.gz
+tar -zxvf v8.0.1095.tar.gz
+
+cd vim-8.0.1095
+./configure --with-features=huge -enable-pythoninterp --enable-python3interp
+make 
+echo '123456' | sudo -S make install
