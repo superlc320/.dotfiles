b5314475da19de3df523fc3fc4dfa3ea567505a7
superlc320 <luocan14@gmail.com>
2018-01-15 23:49:02 +0800

    123

--- add_softlinks.sh
+++ add_softlinks.sh
@@ -2,3 +2,4 @@
 
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.ycm_extra_conf.py ~/.ycm_extra_conf.py
--- install_vim80.sh
+++ install_vim80.sh
@@ -5,11 +5,13 @@ echo '123456' | sudo -S yum -y install ncurses-devel python-devel ruby-devel lua
 mkdir -p ~/download
 cd ~/download
 
-wget -c https://github.com/vim/vim/archive/v8.0.1095.tar.gz
-tar -zxvf v8.0.1095.tar.gz
+wget -c https://github.com/vim/vim/archive/v8.0.1428.tar.gz
+tar xzvf v8.0.1428.tar.gz
 
-cd vim-8.0.1095
+cd vim-8.0.1428
 make distclean
-CFLAGS="-I/usr/lib64/perl5/CORE/" ./configure --with-features=huge --enable-pythoninterp --enable-rubyinterp --enable-luainterp --enable-perlinterp
+# CFLAGS="-I/usr/lib64/perl5/CORE/" 
+# ./configure --prefix=/export/luocan/path/to --with-features=huge --enable-pythoninterp --enable-rubyinterp --enable-luainterp --enable-perlinterp
+./configure --prefix=/export/luocan/path/to --with-features=huge --enable-pythoninterp --enable-rubyinterp --enable-luainterp
 make 
-echo '123456' | sudo -S make install
+make install
