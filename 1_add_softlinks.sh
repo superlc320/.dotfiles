7f1b3b823eac6af8b26d6c11fe6683a27994d3e0
superlc320 <luocan14@gmail.com>
2018-01-17 10:15:15 +0800

    123

--- 1_add_softlinks.sh
+++ 1_add_softlinks.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+rm ~/.gitconfig
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+
+rm ~/.vimrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
+
+rm ~/.ycm_extra_conf.py
 ln -s ~/.dotfiles/.ycm_extra_conf.py ~/.ycm_extra_conf.py
+
+rm ~/.zshrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
