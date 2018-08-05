50e9783a228e4cbccf152f56e9acffa0836284bd
superlc320 <luocan14@gmail.com>
2018-08-05 15:56:53 +0800

    用homebrew安装astyle

--- 5_install_astyle.sh
+++ 5_install_astyle.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-mkdir -p ~/download
-cd ~/download
-wget -c https://downloads.sourceforge.net/project/astyle/astyle/astyle%203.0.1/astyle_3.0.1_linux.tar.gz
-tar xzvf astyle_3.0.1_linux.tar.gz
-cd astyle/build/gcc
-make prefix=/export/luocan/path/to
-make install prefix=/export/luocan/path/to
+brew install astyle
