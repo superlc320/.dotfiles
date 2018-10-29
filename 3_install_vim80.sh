e88c28c264de2565be17ab74d1b62e9e1f7ecf1f
superlc320 <luocan14@gmail.com>
2018-10-29 17:33:10 +0800

    2

--- 3_install_vim80.sh
+++ 3_install_vim80.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-echo '123456' | sudo -S yum -y install ncurses-devel python-devel ruby-devel lua-devel  perl-devel
+echo '112358' | sudo -S yum -y install ncurses-devel python-devel ruby-devel lua-devel  perl-devel
 
 mkdir -p ~/path/to
 mkdir -p ~/download
 cd ~/download
 
-wget -c https://github.com/vim/vim/archive/v8.0.1428.tar.gz
-tar xzvf v8.0.1428.tar.gz
+wget -c https://github.com/vim/vim/archive/v8.1.0500.tar.gz
+tar xzvf v8.1.0500.tar.gz
 
-cd vim-8.0.1428
+cd vim-8.1.0500
 make distclean
 # CFLAGS="-I/usr/lib64/perl5/CORE/" 
 # ./configure --prefix=/export/luocan/path/to --with-features=huge --enable-pythoninterp --enable-rubyinterp --enable-luainterp --enable-perlinterp
