a23a71ea4d1ebd4649455fb527feb26b1cbe8080
superlc320 <luocan14@gmail.com>
2018-11-14 03:09:08 +0800

    升级astyle到3.1版本

--- 5_install_astyle.sh
+++ 5_install_astyle.sh
@@ -2,8 +2,8 @@
 
 mkdir -p ~/download
 cd ~/download
-wget -c https://downloads.sourceforge.net/project/astyle/astyle/astyle%203.0.1/astyle_3.0.1_linux.tar.gz
-tar xzvf astyle_3.0.1_linux.tar.gz
+wget -c https://downloads.sourceforge.net/project/astyle/astyle/astyle%203.1/astyle_3.1_linux.tar.gz
+tar xzvf astyle_3.1_linux.tar.gz
 cd astyle/build/gcc
 make prefix=/export/luocan/path/to
 make install prefix=/export/luocan/path/to
