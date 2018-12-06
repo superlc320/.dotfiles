f606fefbc2fded543d714297318488a100670dec
superlc320 <luocan14@gmail.com>
2018-12-06 16:13:06 +0800

    add softlinks 使用 rm -f

--- 1_add_softlinks.sh
+++ 1_add_softlinks.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-rm ~/.gitconfig
+rm -f ~/.gitconfig
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
-rm ~/.vimrc
+rm -f ~/.vimrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 
-rm ~/.ycm_extra_conf.py
+rm -f ~/.ycm_extra_conf.py
 ln -s ~/.dotfiles/.ycm_extra_conf.py ~/.ycm_extra_conf.py
 
-rm ~/.zshrc
+rm -f ~/.zshrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
