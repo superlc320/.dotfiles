220bb09d9279606c84330ff79777c68aae5f4415
superlc320 <luocan14@gmail.com>
2019-04-03 15:02:55 +0800

    将ln -s改为ln -sf

--- 1_add_softlinks.sh
+++ 1_add_softlinks.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-rm -f ~/.gitconfig
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-
-rm -f ~/.vimrc
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
-
-rm -f ~/.ycm_extra_conf.py
-ln -s ~/.dotfiles/.ycm_extra_conf.py ~/.ycm_extra_conf.py
-
-rm -f ~/.zshrc
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/.ycm_extra_conf.py ~/.ycm_extra_conf.py
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
