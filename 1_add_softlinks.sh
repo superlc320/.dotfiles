fbacfb2fdce5371f137614c3293ed7864dcc2dd0
superlc320 <luocan14@gmail.com>
2022-06-20 04:04:46 -0400

    拆分softlink命令

--- 1_add_softlinks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/.dotfiles/.vimrc ~/.vimrc
-ln -sf ~/.dotfiles/.ycm_extra_conf.py ~/.ycm_extra_conf.py
---
+++ link_ycm_extra_conf.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ln -sf ~/.dotfiles/.ycm_extra_conf.py ~/.ycm_extra_conf.py
