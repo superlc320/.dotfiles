dfe205584bb697b0f99bbf6b8b5d931e7ef6f465
superlc320 <luocan14@gmail.com>
2022-06-20 07:00:08 -0400

    不再安装astyle，改用clang-format

--- 5_install_astyle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/download
-cd ~/download
-wget -c https://downloads.sourceforge.net/project/astyle/astyle/astyle%203.1/astyle_3.1_linux.tar.gz
-tar xzvf astyle_3.1_linux.tar.gz
-cd astyle/build/gcc
-make prefix=/export/luocan/path/to
-make install prefix=/export/luocan/path/to
